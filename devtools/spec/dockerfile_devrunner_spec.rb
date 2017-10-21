25b7c24dadc1ce93ec91e0dfab66248d869bb022
faustocv <fausto.castaneda.v@gmail.com>
2017-10-21 16:21:31 -0500

    Adds devtools folder and devrunner image

---
+++ devtools/spec/dockerfile_devrunner_spec.rb
@@ -0,0 +1,34 @@
+# spec/dockerfile_dev_spec.rb
+
+require 'serverspec'
+require 'docker-api'
+require 'json'
+
+describe 'Dockerfile.devrunner' do
+  before(:all) do
+    @image = Docker::Image.build_from_dir('.', {'dockerfile' => 'Dockerfile.devrunner'}) do |v|
+      if ( log = JSON.parse(v) ) && log.has_key?("stream")
+        $stdout.puts log['stream']
+      end
+    end
+    @image.tag('repo' => 'jammpaz/devrunner', 'tag' => '0.0.1')
+    set :backend, :docker
+    set :os, family: :debian
+    set :docker_image, @image.id
+  end
+
+  describe 'Dependencies' do
+    context 'ruby' do
+      it 'has to be installed' do
+        response = command('sh -c "ruby -v"')
+        expect(response.stdout).to match '2.4.2'
+      end
+    end
+    describe package('jekyll') do
+        it { should be_installed.by('gem') }
+    end
+    describe package('bundler') do
+        it { should be_installed.by('gem') }
+    end
+  end
+end
