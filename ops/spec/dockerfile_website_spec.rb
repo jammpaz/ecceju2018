f741b8f12ed3b05d28d6ad942e392b86390e7e17
faustocv <fausto.castaneda.v@gmail.com>
2017-11-18 18:16:46 -0500

    Configura el artefacto usando nginx server

--- ops/spec/dockerfile_website_spec.rb
+++ ops/spec/dockerfile_website_spec.rb
@@ -13,23 +13,34 @@ describe 'Dockerfile.website' do
     end
     @image.tag('repo' => 'jammpaz/website', 'tag' => '0.0.1')
     set :backend, :docker
-    set :os, family: :redhat
     set :docker_image, @image.id
-  end
-
-  describe 'Dependencies' do
-    describe package('httpd') do
-        it { should be_installed }
-    end
   end
 
   describe 'Configuration' do
     describe port(80) do
       it { should be_listening }
     end
+  end
 
-    describe user('website') do
-      it { should exist }
+  describe 'Website content' do
+    describe file('/usr/share/nginx/html/index.html') do
+        it { should exist }
+    end
+
+    describe file('/usr/share/nginx/html/feed.xml') do
+        it { should exist }
+    end
+
+    describe file('/usr/share/nginx/html/404.html') do
+        it { should exist }
+    end
+
+    describe file('/usr/share/nginx/html/about') do
+      it { should be_directory }
+    end
+
+    describe file('/usr/share/nginx/html/assets') do
+      it { should be_directory }
     end
   end
 end
