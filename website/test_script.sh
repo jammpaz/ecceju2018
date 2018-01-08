200b60835d684df516a6d57913fb86cf6d1f14e5
faustocv <fausto.castaneda.v@gmail.com>
2018-01-07 22:47:26 -0500

    [#1] Adds materialize theme

---
+++ website/test_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+REPO_TAG='jammpaz/devrunner:0.0.1'
+
+docker run --rm \
+           -v $(pwd)/website/:/website \
+           -w /website \
+           -v gems_data:/usr/local/bundle/ \
+           $REPO_TAG \
+           sh -c "jekyll clean && jekyll build"
